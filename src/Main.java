4b6ec19c309fdbdc9f6b87556b2d496a19bb51d2
Maksim <raymax.spb@gmail.com>
2024-02-21 20:45:47 +0300

    Домашнее задание 4

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Task №1");
-        int
+        int ageAdultHuman = 23;
+        if(ageAdultHuman>=18){
+            System.out.println("Если возраст человека равен "
+            +ageAdultHuman+ " то он совершеннолетний ");
+        }
     }
 
 }
